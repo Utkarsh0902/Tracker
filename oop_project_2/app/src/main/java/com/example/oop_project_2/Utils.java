db349a68968e6c0ce9cfd94f9ee9eb5bd19ace50
Utkarsh0902 <f20180456@hyderabad.bits-pilani.ac.in>
2021-02-20 15:53:28 +0530

    Revert "Added files for the first time"
    This reverts commit ca3b1904ec4815ed62cc4c5c998c738f4ea869b9.

--- oop_project_2/app/src/main/java/com/example/oop_project_2/Utils.java
+++
@@ -1,7 +0,0 @@
-package com.example.oop_project_2;
-
-public class Utils {
-    //Automated email and pass id go here
-    public static final String EMAIL = "tanmaychowhan@gmail.com";
-    public static final String PASSWORD = "dgveptqjnagucihc";
-}
